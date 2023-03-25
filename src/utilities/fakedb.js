9e298c1c41f40632b8244d37908a9b74956e18dd
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-25 23:36:34 +0600

    updated cart, item is adding perfectly

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,10 +1,12 @@
 // To Manage Local Storage cart Data :
 const addToDb = id => {
     const quantity = localStorage.getItem(id);
-    
+
         if(quantity){
 
             console.log('already there');
+            const newQuantity = parseInt(quantity) + 1;
+            localStorage.setItem(id, newQuantity);
 
         } else {
             console.log('new Item added');
