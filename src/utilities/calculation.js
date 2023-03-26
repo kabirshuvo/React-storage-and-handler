7bf6cca30a865f92d0b49ddb2b9f196ccb2c4c45
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-26 18:01:04 +0600

    Implementing another reduce system

--- src/utilities/calculation.js
+++ src/utilities/calculation.js
@@ -6,12 +6,21 @@ const multiply =(first, second) =>{
     return first * second;
 }
 
-const number = [10, 20, 30, 40, 50];
-const sumReducer = (previous, current) => previous + current;
-const total = number.reduce(sumReducer ,0)
+// const number = [10, 20, 30, 40, 50];
+// const sumReducer = (previous, current) => previous + current;
+// const total = number.reduce(sumReducer ,0)
 
+// console.log(total);
+
+  const items = [
+    {id: 1, name: 'jama', price: 500},
+    {id: 2, name: 'tama', price: 600},
+    {id: 3, name: 'pajama', price: 700},
+    {id: 4, name: 'jamama', price: 800},
+  ]
+const itemReducer = (previous, current) => previous + current.price;
+const total = items.reduce( itemReducer, 0 )
 console.log(total);
-
 
 export { add, multiply };
 
